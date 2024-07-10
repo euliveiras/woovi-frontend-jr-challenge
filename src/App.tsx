171110ddf6d100d7668387781ff0b8a657fd3e85
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-10 14:26:44 -0300

    style: FirstStep header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { CustomFooter } from "./components/custom-footer";
 import { CustomHeader } from "./components/custom-header";
+import { FirstStep } from "./components/first-step";
 
 function App() {
   return (
     <div className="flex h-dvh w-full flex-col items-center justify-center bg-gray-950 p-8">
       <main className="relative size-full max-w-lg bg-slate-100">
         <CustomHeader />
+        <FirstStep />
         <CustomFooter />
       </main>
     </div>
---
+++ src/components/first-step.tsx
@@ -0,0 +1,7 @@
+export function FirstStep() {
+  return (
+    <section className="flex size-full flex-col items-center p-4">
+      <header className="text-lg font-bold">João, como você quer pagar?</header>
+    </section>
+  );
+}
