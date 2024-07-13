48761d91816d94432976efd9fb97223b2e445ff1
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-13 18:29:36 -0300

    build: gitignore

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import http from "http";
 import { router } from "./router";
 
-const server = express();
+const app = express();
+const server = http.createServer(app)
 
 const PORT = process.env.PORT ?? 3012;
 
-server.use("/", router)
+app.use("/", router)
 
 server.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
