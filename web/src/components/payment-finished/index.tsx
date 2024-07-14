c89ebb5e95a2bee7507b0632fe78169bee40c3e7
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-14 16:08:02 -0300

    styles: changed font weight in PaymentFinished

--- web/src/components/payment-finished/index.tsx
+++ web/src/components/payment-finished/index.tsx
@@ -2,7 +2,7 @@ export function PaymentFinished() {
   return (
     <div className="grid size-full place-content-center place-items-center gap-3">
       <p className="text-2xl">Seu pagamento foi feito!</p>
-      <p className="text-xl text-custom-green-400">
+      <p className="text-xl font-bold text-custom-green-400">
         Obrigado! <strong>(＾▽＾)</strong>
       </p>
     </div>
