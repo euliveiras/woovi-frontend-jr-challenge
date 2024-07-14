a94711abbb4a3b16889bac8126901e4e24c9b7fc
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-13 21:56:25 -0300

    feat: added react-query provider

--- web/src/components/mock-payment.tsx
+++ web/src/components/mock-payment.tsx
@@ -1,15 +1,20 @@
 import { Button } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
 import { formatPrice } from "../utils/format-price";
+import { useMutation } from "@tanstack/react-query";
 
 export function MockPayment() {
   const [searchParams] = useSearchParams();
+  const mutation = useMutation({
+    mutationFn: () => {
+      return fetch(new URL(import.meta.env.VITE_SOCKET_URL) + "mock-payment");
+    },
+  });
+
   const value = searchParams.get("value");
   const currency = searchParams.get("currency");
 
-  const onConfirm = async () => {
-    await fetch(new URL(import.meta.env.VITE_SOCKET_URL) + "mock-payment");
-  };
+  const onConfirm = async () => mutation.mutate();
 
   return (
     <div className="grid size-full place-content-center place-items-center gap-4 pb-12">
@@ -19,8 +24,12 @@ export function MockPayment() {
           {formatPrice({ value: Number(value), currency })}
         </strong>
       </p>
-      <Button onClick={onConfirm} variant="contained">
-        Confirmar pagamento
+      <Button
+        disabled={mutation.isPending || mutation.isSuccess}
+        onClick={onConfirm}
+        variant="contained"
+      >
+        {mutation.isSuccess ? "Pagamento confirmado" : "Confirmar pagamento"}
       </Button>
     </div>
   );
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,7 +7,9 @@ import { theme } from "./theme.ts";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Payment } from "./components/payment.tsx";
 import { MockPayment } from "./components/mock-payment.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
     path: "/",
@@ -28,7 +30,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
