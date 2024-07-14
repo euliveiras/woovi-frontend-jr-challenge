4686a8189fca37abbf9ab5fc2121c918487ec349
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-14 11:28:40 -0300

    style: changed TextField label color

--- web/src/components/payment-pix-credit-card/custom-input.tsx
+++ web/src/components/payment-pix-credit-card/custom-input.tsx
@@ -1,5 +1,11 @@
 import TextField, { TextFieldProps } from "@mui/material/TextField";
 
 export function CustomInput(props: TextFieldProps) {
-  return <TextField variant="outlined" {...props} />;
+  return (
+    <TextField
+      InputLabelProps={{ sx: { color: "#4D4D4D" } }}
+      variant="outlined"
+      {...props}
+    />
+  );
 }
