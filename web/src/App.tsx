63c3b0669770f49dabaec1abf229bf0ef5ee0e33
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-15 16:25:38 -0300

    style: changed bg and padding from container

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,8 +12,8 @@ function App() {
   }, [navigate, searchParams]);
 
   return (
-    <div className="flex h-dvh w-full flex-col items-center justify-center bg-gray-950 px-8 py-4">
-      <main className="grid size-full max-w-sm grid-rows-[auto_1fr_auto] gap-1 bg-slate-100 p-4">
+    <div className="flex h-dvh w-full flex-col items-center justify-center bg-slate-100 p-4">
+      <main className="grid size-full max-w-sm grid-rows-[auto_1fr_auto] gap-1 p-4">
         <CustomHeader />
         <section className="flex size-full flex-col items-center overflow-hidden p-4 pb-8">
           <Outlet />
