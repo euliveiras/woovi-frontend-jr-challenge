426c8317060c62175dea18cdcfcb7acd85f8eaae
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-15 16:35:34 -0300

    refactor: moved router to own module

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,28 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
-import App from "./App.tsx";
 import "./index.css";
 import { theme } from "./theme.ts";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Payment } from "./components/payment.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PageNotFound } from "./page-not-found";
+import { router } from "./router.tsx";
+import { RouterProvider } from "react-router-dom";
 
 const queryClient = new QueryClient();
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <PageNotFound />,
-    children: [
-      {
-        path: "payment",
-        element: <Payment />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
