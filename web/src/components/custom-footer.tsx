a2d55e4ea63bf33577f871bb906dba82a36c4caa
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-15 16:29:07 -0300

    style: changed footer font size

--- web/src/components/custom-footer.tsx
+++ web/src/components/custom-footer.tsx
@@ -6,7 +6,7 @@ export function CustomFooter() {
     <footer>
       <span className="flex items-center justify-center gap-1 text-gray-400">
         <GppGoodIcon />
-        <p className="text-sm font-semibold">Pagamento 100% seguro via: </p>
+        <p className="text-xs font-semibold">Pagamento 100% seguro via: </p>
         <WooviGrayLogo />
       </span>
     </footer>
