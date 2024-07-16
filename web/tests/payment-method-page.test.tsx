e1af5153ced8cb3b1d25e39f014e274c5ff2dbf9
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-15 22:47:32 -0300

    test: added forEach to test each value

--- web/tests/payment-method-page.test.tsx
+++ web/tests/payment-method-page.test.tsx
@@ -3,9 +3,11 @@ import { render, screen } from "./test-utils";
 import { PaymentMethod } from "../src/components/payment-method";
 import { MemoryRouter } from "react-router-dom";
 import { formatPrice } from "../src/utils/format-price";
+import { calculateFee } from "../src/utils/calculate-fee";
 
 describe("Payment method page", async () => {
   const value = 13909123;
+  const installMents = [1, 2, 3, 4, 5, 6, 7];
   const currency = "BRL";
   beforeAll(() => {
     render(
@@ -16,10 +18,21 @@ describe("Payment method page", async () => {
       </MemoryRouter>,
     );
   });
-  test(`it should contain a p element with ${formatPrice({ value, currency })} text`, () => {
-    const element = screen.getByText(formatPrice({ value, currency }), {
-      collapseWhitespace: false,
+  installMents.forEach((installment) => {
+    test(`it should contain a p element with ${formatPrice({
+      value: calculateFee(String(value), 2) / installment,
+      currency,
+    })} text`, () => {
+      const element = screen.getByText(
+        formatPrice({
+          value: calculateFee(String(value), installment) / installment,
+          currency,
+        }),
+        {
+          collapseWhitespace: false,
+        },
+      );
+      expect(element).toBeInTheDocument();
     });
-    expect(element).toBeInTheDocument();
   });
 });
