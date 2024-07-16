377c7422f7e5f58128dbbdb2e48bfa3c13bec80b
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-16 17:03:16 -0300

    style: changed color of select and options

--- web/src/components/payment-pix-credit-card/custom-select.tsx
+++ web/src/components/payment-pix-credit-card/custom-select.tsx
@@ -2,6 +2,7 @@ import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import { useSearchParams } from "react-router-dom";
 import { formatPrice } from "../../utils/format-price";
+import { theme } from "../../theme";
 
 type CustomSelectProps = { name: string; value: number; installment: number };
 
@@ -10,10 +11,18 @@ export function CustomSelect({ name, installment, value }: CustomSelectProps) {
   const currency = searchParams.get("currency");
   const arr = Array.from(Array(installment).keys()).map((n) => ++n);
   return (
-    <Select name={name} defaultValue={1}>
+    <Select
+      name={name}
+      defaultValue={1}
+      sx={{ color: theme.custom.color.gray }}
+    >
       {arr.map((val) => {
         return (
-          <MenuItem key={val} value={val}>
+          <MenuItem
+            sx={{ color: theme.custom.color.gray }}
+            key={val}
+            value={val}
+          >
             {val}x{" "}
             {formatPrice({ value: Math.round(Number(value) / val), currency })}
           </MenuItem>
