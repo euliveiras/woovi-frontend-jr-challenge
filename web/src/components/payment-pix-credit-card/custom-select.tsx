e2cfa0db7c2c8523f92bbc9346b4619484d98359
Matheus Oliveira <matheus.skm@hotmail.com>
2024-07-17 16:19:31 -0300

    refactor: added selectProps in CustomSelect

--- web/src/components/payment-pix-credit-card/custom-select.tsx
+++ web/src/components/payment-pix-credit-card/custom-select.tsx
@@ -1,20 +1,27 @@
 import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
+import Select, { SelectProps } from "@mui/material/Select";
 import { useSearchParams } from "react-router-dom";
 import { formatPrice } from "../../utils/format-price";
 import { theme } from "../../theme";
 
-type CustomSelectProps = { name: string; value: number; installment: number };
+type CustomSelectProps = {
+  selectProps: SelectProps;
+  value: number;
+  installment: number;
+};
 
-export function CustomSelect({ name, installment, value }: CustomSelectProps) {
+export function CustomSelect({
+  installment,
+  value,
+  selectProps,
+}: CustomSelectProps) {
   const [searchParams] = useSearchParams();
   const currency = searchParams.get("currency");
   const arr = Array.from(Array(installment).keys()).map((n) => ++n);
   return (
     <Select
-      name={name}
-      defaultValue={1}
       sx={{ color: theme.custom.color.gray }}
+      {...selectProps}
     >
       {arr.map((val) => {
         return (
